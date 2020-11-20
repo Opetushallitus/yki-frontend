ce55fd8b8d24b684300d9780f78c50421558481e
mikkorantanen <mikko.rantanen@siili.com>
2020-11-20 15:13:24 +0200

    MAjor refactoring on examdates

--- src/main/js/src/containers/ExamDates/ExamDateModalContent/AddOrEditExamDate.js
+++ src/main/js/src/containers/ExamDates/ExamDateModalContent/AddOrEditExamDate.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {withTranslation} from 'react-i18next';
 import PropTypes from "prop-types";
 import DatePicker from '../../../components/UI/DatePicker/DatePicker';
-import RegistrationDatesSelector from "../RegistrationPeriodSelector/RegistrationPeriodSelector";
 import {languageToString, levelTranslations} from "../../../util/util";
 import {LANGUAGES} from "../../../common/Constants";
 import closeOverlay from '../../../assets/svg/close-overlay.svg';
@@ -11,221 +10,218 @@ import ToggleSwitch from "../../../components/UI/ToggleSwitch/ToggleSwitch";
 import moment from "moment";
 
 const AddOrEditExamDate = (props) => {
-    const {examDates, examModal, onIndexSelect, onUpdate, t} = props;
-    const currentDate = new Date();
+  const {examDates, onUpdate, t} = props;
+  const currentDate = new Date();
 
-    const initializeLanguageArray = () => {
-        let languageArray = [];
-        if (examDates.languages && examDates.languages.length > 0) {
-            examDates.languages.map((item) => {
-                let language = languageToString(item.language_code);
-                //TODO: add correct level when provided from backend
-                let level = t(levelTranslations.PERUS)
-                return languageArray.push({language, level});
-            });
-            return languageArray;
-        } else return [];
-    }
+  const initializeLanguageArray = () => {
+    let languageArray = [];
+    if (examDates.languages && examDates.languages.length > 0) {
+      examDates.languages.map((item) => {
+        let language = languageToString(item.language_code);
+        //TODO: add correct level when provided from backend
+        let level = t(levelTranslations.PERUS)
+        return languageArray.push({language, level});
+      });
+      return languageArray;
+    } else return [];
+  }
 
-    // useState
-    const [languageAndLevel, setLanguageAndLevel] = useState(initializeLanguageArray);
-    const [level, setLevel] = useState(t(levelTranslations.PERUS));
-    const [language, setLanguage] = useState(LANGUAGES[0].name);
-    const [disabled, setDisabled] = useState(true);
+  // useState
+  const [languageAndLevel, setLanguageAndLevel] = useState(initializeLanguageArray);
+  const [level, setLevel] = useState(t(levelTranslations.PERUS));
+  const [language, setLanguage] = useState(LANGUAGES[0].name);
+  const [disabled, setDisabled] = useState(true);
 
-    // language levels
-    const PERUS = t(levelTranslations.PERUS);
-    const KESKI = t(levelTranslations.KESKI);
-    const YLIN = t(levelTranslations.YLIN);
+  // language levels
+  const PERUS = t(levelTranslations.PERUS);
+  const KESKI = t(levelTranslations.KESKI);
+  const YLIN = t(levelTranslations.YLIN);
 
-    const handleRemoveLanguage = item => {
-        const temp = [...languageAndLevel];
-        temp.splice(item, 1);
-        setLanguageAndLevel(temp);
-    }
+  const handleRemoveLanguage = item => {
+    const temp = [...languageAndLevel];
+    temp.splice(item, 1);
+    setLanguageAndLevel(temp);
+  }
 
-    const languagesList = (
-        <>
-            {languageAndLevel.map((item, i) => {
-                return (
-                    <span key={i}>
+  const languagesList = (
+    <>
+      {languageAndLevel.map((item, i) => {
+        return (
+          <span key={i}>
           <img src={closeOverlay} alt={'delete'} onClick={() => handleRemoveLanguage(i)}/>
           <p style={{marginLeft: '10px'}}>{item.language}, {item.level}</p>
          </span>
-                )
-            })}
-        </>
-    );
+        )
+      })}
+    </>
+  );
 
-    //TODO handle onChange
-    const examTimeGrid = (
-        <div className={classes.TimeGrid}>
-            <div>
-                <label>{t('examDates.choose.registrationTime')}</label>
-                <RegistrationDatesSelector registrationPeriods={examDates} onIndexSelect={onIndexSelect}
-                                           stateItem={''}/>
-            </div>
-            <div>
-                <label>{t('examDates.choose.examDate')}</label>
-                <div className={classes.DatePickerWrapper}>
-                    <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
-                </div>
-            </div>
+  //TODO handle onChange
+  const examTimeGrid = (
+    <div className={classes.TimeGrid}>
+      <div>
+        <label>{t('examDates.choose.registrationTime')}</label>
+        <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
+      </div>
+      <div>
+        <label>{t('examDates.choose.examDate')}</label>
+        <div className={classes.DatePickerWrapper}>
+          <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
         </div>
-    );
+      </div>
+    </div>
+  );
 
-    // TODO: new localizations to be added!
-    const registrationTimeGrid = (
-        <div className={classes.TimeGrid}>
-            <div>
-                <label>{t('examDates.choose.registrationTime')}</label>
-                <div className={classes.DatePickerWrapper}>
-                    <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
-                </div>
-            </div>
-            <div>
-                <label>{t('examDates.choose.registrationTime')}</label>
-                <div className={classes.DatePickerWrapper}>
-                    <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
-                </div>
-            </div>
+  // TODO: new localizations to be added!
+  const registrationTimeGrid = (
+    <div className={classes.TimeGrid}>
+      <div>
+        <label>{t('examDates.choose.registrationTime')}</label>
+        <div className={classes.DatePickerWrapper}>
+          <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
         </div>
-    );
-
-    const languageAndLevelGrid = (
-        <div className={classes.LanguageAndLevelGrid}>
-            <div>
-                <label>{t('examDates.choose.examLanguage')}</label>
-                <select
-                    className={classes.ExamLevels} defaultValue={language}
-                    onChange={e => setLanguage(e.target.value)}
-                >
-                    {LANGUAGES.map((lang) => {
-                        return <option key={lang.code} value={lang.name}>{lang.name}</option>
-                    })}
-                </select>
-            </div>
-            <div>
-                <label>{t('registration.select.level')}</label>
-                <select className={classes.ExamLevels} defaultValue={level} onChange={e => setLevel(e.target.value)}>
-                    <option value={PERUS}>{PERUS}</option>
-                    <option value={KESKI}>{KESKI}</option>
-                    <option value={YLIN}>{YLIN}</option>
-                </select>
-            </div>
-            <div>
-                <button
-                    className={classes.LanguageAddition}
-                    onClick={() => setLanguageAndLevel(prev => [...prev, {language, level}])}
-                >
-                    {t('examDates.addNew.addLanguage')}
-                </button>
-            </div>
+      </div>
+      <div>
+        <label>{t('examDates.choose.registrationTime')}</label>
+        <div className={classes.DatePickerWrapper}>
+          <DatePicker options={{defaultDate: currentDate}} onChange={() => console.log('selected new date')}/>
         </div>
-    );
+      </div>
+    </div>
+  );
 
-    const postAdmissionToggle = (
-        <>
-            <label>{'Järjestäjien jälki-ilmoittautuminen'}</label>
-            <div className={classes.PostAdmissionContainer}>
-                <div className={classes.Toggle}>
-                    <ToggleSwitch onChange={() => setDisabled(!disabled)}/>
-                    <p>{'Ei avattu'}</p>
-                </div>
-                <p className={classes.Label}>Päivämäärät</p>
-                <div className={disabled ? classes.DisabledPicker : classes.DatePickerWrapper}>
-                    <DatePicker disabled={disabled} options={{defaultDate: currentDate}}
-                                onChange={() => console.log('changed')}/>
-                </div>
-                <div className={disabled ? classes.DisabledPicker : classes.DatePickerWrapper}>
-                    <DatePicker
-                        id="postAdmissionEnd"
-                        disabled={disabled}
-                        options={{
-                            defaultDate: examDates.post_admission_end_date,
-                            // value: values.postAdmissionEnd,
-                            minDate: moment(examDates.registration_end_date).add(1, 'days').format('YYYY MM DD'),
-                            maxDate: moment(examDates.exam_date).add(-1, 'days').format('YYYY MM DD')
-                        }}
-                        onChange={d =>
-                            console.log(
-                                'postAdmissionEnd: ',
-                                moment(d[0]).format('YYYY-MM-DD'),
-                            )
-                        }
-                        locale={props.i18n.language}
-                        tabIndex="1"
-                    />
-                </div>
-            </div>
-        </>
-    );
+  const languageAndLevelGrid = (
+    <div className={classes.LanguageAndLevelGrid}>
+      <div>
+        <label>{t('examDates.choose.examLanguage')}</label>
+        <select
+          className={classes.ExamLevels} defaultValue={language}
+          onChange={e => setLanguage(e.target.value)}
+        >
+          {LANGUAGES.map((lang) => {
+            return <option key={lang.code} value={lang.name}>{lang.name}</option>
+          })}
+        </select>
+      </div>
+      <div>
+        <label>{t('registration.select.level')}</label>
+        <select className={classes.ExamLevels} defaultValue={level} onChange={e => setLevel(e.target.value)}>
+          <option value={PERUS}>{PERUS}</option>
+          <option value={KESKI}>{KESKI}</option>
+          <option value={YLIN}>{YLIN}</option>
+        </select>
+      </div>
+      <div>
+        <button
+          className={classes.LanguageAddition}
+          onClick={() => setLanguageAndLevel(prev => [...prev, {language, level}])}
+        >
+          {t('examDates.addNew.addLanguage')}
+        </button>
+      </div>
+    </div>
+  );
 
-    const editView = () => (
-        <div>
-            <h3 style={{marginBlockStart: '0'}}>Muokkaa aikaa</h3>
-            <div className={classes.Form}>
-                <div className={classes.EditViewTop}>
-                    <div>
-                        <label>{t('examDates.choose.examDate')}</label>
-                        <div className={classes.DatePickerWrapper}>
-                            <DatePicker options={{defaultDate: currentDate}}
-                                        onChange={() => console.log('selected new date')}/>
-                        </div>
-                    </div>
-                    <div>
-                        {postAdmissionToggle}
-                    </div>
-                </div>
-                {languageAndLevelGrid}
-                <div className={classes.AddedLanguages}>
-                    {languagesList}
-                </div>
-            </div>
-            <button
-                className={classes.AdditionButton}
-                onClick={() => onUpdate()}
-            >
-                {t('examDates.addNew.confirm')}
-            </button>
+  const postAdmissionToggle = (
+    <>
+      <label>{'Järjestäjien jälki-ilmoittautuminen'}</label>
+      <div className={classes.PostAdmissionContainer}>
+        <div className={classes.Toggle}>
+          <ToggleSwitch onChange={() => setDisabled(!disabled)}/>
+          <p>{'Ei avattu'}</p>
         </div>
-    );
+        <p className={classes.Label}>Päivämäärät</p>
+        <div className={disabled ? classes.DisabledPicker : classes.DatePickerWrapper}>
+          <DatePicker disabled={disabled} options={{defaultDate: currentDate}}
+                      onChange={() => console.log('changed')}/>
+        </div>
+        <div className={disabled ? classes.DisabledPicker : classes.DatePickerWrapper}>
+          <DatePicker
+            id="postAdmissionEnd"
+            disabled={disabled}
+            options={{
+              defaultDate: examDates.post_admission_end_date,
+              // value: values.postAdmissionEnd,
+              minDate: moment(examDates.registration_end_date).add(1, 'days').format('YYYY MM DD'),
+              maxDate: moment(examDates.exam_date).add(-1, 'days').format('YYYY MM DD')
+            }}
+            onChange={d =>
+              console.log(
+                'postAdmissionEnd: ',
+                moment(d[0]).format('YYYY-MM-DD'),
+              )
+            }
+            locale={props.i18n.language}
+            tabIndex="1"
+          />
+        </div>
+      </div>
+    </>
+  );
 
-    //TODO: move into form and add validation : Formik & yup
-    const createView = () => (
-        <>
-            <h3 style={{marginBlockStart: '0'}}>{t('examDates.addNew.examDate')}</h3>
-            <div className={classes.Form}>
-                <>
-                    {examModal ? <>{examTimeGrid}</> : <>{registrationTimeGrid}</>}
-                </>
-                {languageAndLevelGrid}
-                <div className={classes.AddedLanguages}>
-                    {languagesList}
-                </div>
+  const editView = () => (
+    <div>
+      <h3 style={{marginBlockStart: '0'}}>Muokkaa aikaa</h3>
+      <div className={classes.Form}>
+        <div className={classes.EditViewTop}>
+          <div>
+            <label>{t('examDates.choose.examDate')}</label>
+            <div className={classes.DatePickerWrapper}>
+              <DatePicker options={{defaultDate: examDates[0].exam_date}}
+                          onChange={() => console.log('selected new date')}/>
             </div>
-            <button
-                className={classes.AdditionButton}
-                onClick={() => onUpdate()}
-            >
-                {t('examDates.addNew.confirm')}
-            </button>
-        </>
-    );
+          </div>
+          <div>
+            {postAdmissionToggle}
+          </div>
+        </div>
+        {languageAndLevelGrid}
+        <div className={classes.AddedLanguages}>
+          {languagesList}
+        </div>
+      </div>
+      <button
+        className={classes.AdditionButton}
+        onClick={() => onUpdate()}
+      >
+        {t('examDates.addNew.confirm')}
+      </button>
+    </div>
+  );
 
-    return (
-        examDates.length === 1 ? editView() : createView()
-    );
+  //TODO: move into form and add validation : Formik & yup
+  const createView = () => (
+    <>
+      <h3 style={{marginBlockStart: '0'}}>{t('examDates.addNew.examDate')}</h3>
+      <div className={classes.Form}>
+        {examTimeGrid}
+        {registrationTimeGrid}
+        {languageAndLevelGrid}
+        <div className={classes.AddedLanguages}>
+          {languagesList}
+        </div>
+      </div>
+      <button
+        className={classes.AdditionButton}
+        onClick={() => onUpdate()}
+      >
+        {t('examDates.addNew.confirm')}
+      </button>
+    </>
+  );
+
+  return (
+    examDates.length === 1 ? editView() : createView()
+  );
 }
 
 // TODO: add connection to redux when backend is ready
 AddOrEditExamDate.propTypes = {
-    examDates: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.array
-    ]).isRequired,
-    examModal: PropTypes.bool,
-    onUpdate: PropTypes.func.isRequired
+  examDates: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]).isRequired,
+  onUpdate: PropTypes.func.isRequired
 }
 
 export default withTranslation()(AddOrEditExamDate);
--- src/main/js/src/containers/ExamDates/util/RegistrationPeriod.js
+++ src/main/js/src/containers/ExamDates/util/RegistrationPeriod.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import moment from "moment";
 import PropTypes from 'prop-types';
-import {useTranslation} from 'react-i18next';
 
-import {DATE_FORMAT, DATE_FORMAT_WITHOUT_YEAR} from "../../../common/Constants";
+import { DATE_FORMAT } from "../../../common/Constants";
 
 const RegistrationPeriod = ({ period }) => {
-  const [t] = useTranslation();
-  const start = moment(period[0].registration_start_date).format(DATE_FORMAT_WITHOUT_YEAR);
-  const end = moment(period[0].registration_end_date).format(DATE_FORMAT);
+  const start = moment(period.registration_start_date).format(DATE_FORMAT);
+  const end = moment(period.registration_end_date).format(DATE_FORMAT);
 
   return (
     <>
-      {t('common.registationPeriod')}{' '}
       {start}
       &nbsp;
       &ndash;
@@ -23,7 +20,7 @@ const RegistrationPeriod = ({ period }) => {
 };
 
 RegistrationPeriod.propTypes = {
-  period: PropTypes.array.isRequired,
+  period: PropTypes.object.isRequired,
 };
 
 export default RegistrationPeriod;
