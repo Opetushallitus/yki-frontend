d98c57057ef856ce5fda1279a0ba29d79390fc61
Lilla Gerstenbrand <lilla.gerstenbrand@siili.com>
2021-04-22 09:51:35 +0300

    css beautifying for ReEvalForm

--- src/main/js/src/components/Registration/ReEvaluationForm/ReEvaluationForm.js
+++ src/main/js/src/components/Registration/ReEvaluationForm/ReEvaluationForm.js
@@ -1,11 +1,11 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import moment from 'moment';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { connect } from 'react-redux';
 import * as Yup from 'yup';
 
-import { DATE_FORMAT, ISO_DATE_FORMAT_SHORT } from '../../../common/Constants';
+import { DATE_FORMAT } from '../../../common/Constants';
 import { isoFormatDate } from '../../../util/util';
 import classes from './ReEvaluationForm.module.css';
 
@@ -13,6 +13,7 @@ const ReEvaluationForm = props => {
   const { externalState } = props;
   const { t } = useTranslation();
   const mandatoryErrorMsg = t('error.mandatory');
+  const [subtestsFail, setSubTestsFail] = useState(false);
 
   function validateBirthDate(value) {
     if (value) {
@@ -31,20 +32,30 @@ const ReEvaluationForm = props => {
     }
   }
 
+  useEffect(() => {
+    if (externalState.subtests && externalState.subtests.length >= 1)
+      setSubTestsFail(false);
+  }, [externalState.subtests]);
+
   const inputField = (
     name,
     placeholder = '',
     extra,
     type = 'text',
     noLabel = false,
+    style,
   ) => (
-    <>
+    <div className={classes.FieldColumn}>
       {!noLabel && <h3>{t(`registration.form.${name}`)}</h3>}
       <Field
+        onFocus={() => {
+          if (!externalState.subtests || externalState.subtests.length < 1)
+            setSubTestsFail(true);
+        }}
         name={name}
         data-cy={`input-${name}`}
         placeholder={placeholder}
-        className={classes.TextInput}
+        className={style ? style : classes.TextInput}
         type={type}
         aria-label={`registration.form.aria.${name}`}
       />
@@ -55,13 +66,15 @@ const ReEvaluationForm = props => {
         component="span"
         className={classes.ErrorMessage}
       />
-    </>
+    </div>
   );
 
   const validationSchema = Yup.object().shape({
-    email: Yup.string().email(t('error.email')),
-    firstName: Yup.string(),
-    lastName: Yup.string(),
+    email: Yup.string()
+      .email(t('error.email'))
+      .required(mandatoryErrorMsg),
+    firstName: Yup.string().required(mandatoryErrorMsg),
+    lastName: Yup.string().required(mandatoryErrorMsg),
     birthdate: Yup.string().test(
       'invalid-birthdate',
       t('error.birthdate'),
@@ -78,8 +91,6 @@ const ReEvaluationForm = props => {
       ...externalState,
     };
     if (values.birthdate) payload.birthdate = isoFormatDate(values.birthdate);
-
-    console.log('SUBMIT', payload);
   };
 
   return (
@@ -95,24 +106,36 @@ const ReEvaluationForm = props => {
       onSubmit={values => {
         onSubmit(values);
       }}
-      render={({ values, isValid, errors, setFieldValue }) => {
-        console.log(isValid, errors, values);
+      render={({ values, isValid, errors }) => {
         return (
           <Form>
-            {inputField('firstName')}
-            {inputField('lastName')}
-            {inputField('birthdate')}
-            {inputField('email')}
-            {inputField(
-              'consent',
-              '',
-              t('registration.form.consent.confirm'),
-              'checkbox',
-              true,
+            <div className={classes.FieldRow}>
+              {inputField('firstName')}
+              {inputField('lastName')}
+            </div>
+            <div className={classes.FieldRow}>
+              {inputField('birthdate')}
+              {inputField('email')}
+            </div>
+            <div className={classes.FieldRow}>
+              {inputField(
+                'consent',
+                '',
+                t('registration.form.consent.confirm'),
+                'checkbox',
+                true,
+                classes.CheckBoxInput,
+              )}
+            </div>
+            {subtestsFail && (
+              <p className={classes.ErrorMessage}>{t('error.nosubtasks')}</p>
             )}
-
             <button
               role="link"
+              disabled={
+                !isValid ||
+                (externalState.subtests && externalState.subtests.length < 1)
+              }
               className="YkiButton"
               style={{
                 padding: '0.25rem',
