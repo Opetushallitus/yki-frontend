3456def5ca2249f3f944c50b7cfe82f9b7fe6bca
Lilla Gerstenbrand <lilla.gerstenbrand@siili.com>
2021-05-25 13:53:34 +0300

    css variables and fixing level descriptions

--- src/main/js/src/components/UI/Collapsible/Collapsible.js
+++ src/main/js/src/components/UI/Collapsible/Collapsible.js
@@ -1,42 +1,37 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 import classes from './Collapsible.module.css';
 
 const collapsible = props => (
-    <React.Fragment>
-      <div
-          className={[classes.Header, props.className].join(' ')}
-          onClick={props.clicked}
-      >
-        {props.children[0]}
-      </div>
-      {props.extendedClassName ?
-          <div
-              className={props.extendedClassName}
-              style={{
-                display: props.show ? 'block' : 'none',
-              }}
-          >
-            {props.children[1]}
-          </div>
-          :
-      <div
-          style={{
-            display: props.show ? 'block' : 'none',
-          }}
-      >
-        {props.children[1]}
-      </div>
-      }
-    </React.Fragment>
+  <React.Fragment>
+    {/* Button Element */}
+    <div
+      tabIndex={0}
+      role="button"
+      className={[classes.Header, props.className].join(' ')}
+      onClick={props.clicked}
+      onKeyPress={props.clicked}
+    >
+      {props.children[0]}
+    </div>
+    {/* Content */}
+    <div
+      className={`${props.extendedClassName} ${
+        !props.show ? classes.HiddenContent : ''
+      }`}
+      aria-expanded={props.show}
+    >
+      {props.children[1]}
+    </div>
+  </React.Fragment>
 );
 
 collapsible.propTypes = {
   className: PropTypes.string,
   clicked: PropTypes.func.isRequired,
   children: PropTypes.any,
-  extendedClassName: PropTypes.string
+  extendedClassName: PropTypes.string,
 };
 
 export default collapsible;
