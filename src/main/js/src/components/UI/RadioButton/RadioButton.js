ee0a8fe185dd1138369a9f7888864fd4f0737aa0
Mika Huttunen <mika.huttunen@gofore.com>
2021-09-09 10:41:51 +0300

    OPHYKIKEH-7: review fixes

--- src/main/js/src/components/UI/RadioButton/RadioButton.js
+++ src/main/js/src/components/UI/RadioButton/RadioButton.js
@@ -7,7 +7,7 @@ const radioButton = props => (
   <div
     className={[
       classes.RadioButton,
-      !props.disabled ? classes.RadioButtonEnabled : classes.RadioButtonDisabled,
+      props.disabled ? classes.RadioButtonDisabled: classes.RadioButtonEnabled,
     ].join(' ')}
   >
     <input
