81e79232027310ca2610a6288457938fc8aa7e3b
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-02-17 13:47:59 +0200

    OPHYKIKEH-154: Review feedback

--- src/main/js/src/components/ExamSessions/ExamSessions.js
+++ src/main/js/src/components/ExamSessions/ExamSessions.js
@@ -10,43 +10,48 @@ import { examSessionParticipantsCount } from '../../util/examSessionUtil';
 
 export const upcomingAndPastExamSessions = props => {
   const today = moment();
-  const upcomingExamSessions = props.examSessions.filter((es) => today.isSameOrBefore(es.session_date, 'day'));
-  const pastExamSessions = props.examSessions.filter((es) => today.isAfter(es.session_date, 'day'));
-  const renderExamSessionRows = (examSessions) => examSessions.map((e, i) => {
-    const registrationOpen = moment().isSameOrAfter(
-      moment(e.registration_start_date),
-    );
-    const participantsCount = examSessionParticipantsCount(e);
-    return (
-      <div
-        className={classes.Row}
-        key={i}
-        data-cy={`exam-sessions-table-row-${i}`}
-        onClick={() => props.examSessionSelected(e)}
-      >
-        <p>{moment(e.session_date).format(DATE_FORMAT)}</p>
-        <p>{languageToString(e.language_code).toLowerCase()}</p>
-        <p>{levelDescription(e.level_code).toLowerCase()}</p>
-        <p className={classes.DesktopOnly}>
-          {moment(e.registration_start_date).format(DATE_FORMAT_WITHOUT_YEAR)}
-          &ndash;
-          {moment(e.registration_end_date).format(DATE_FORMAT)}
-        </p>
-        <p>
-          {registrationOpen
-            ? `${participantsCount.participants} / ${participantsCount.maxParticipants}`
-            : '-'}
-        </p>
-      </div>
-    );
-  });
+  const upcomingExamSessions = props.examSessions.filter(es =>
+    today.isSameOrBefore(es.session_date, 'day'),
+  );
+  const pastExamSessions = props.examSessions.filter(es =>
+    today.isAfter(es.session_date, 'day'),
+  );
+
+  const renderExamSessionRows = examSessions =>
+    examSessions.map((e, i) => {
+      const registrationOpen = moment().isSameOrAfter(
+        moment(e.registration_start_date),
+      );
+      const participantsCount = examSessionParticipantsCount(e);
+
+      return (
+        <div
+          className={classes.Row}
+          key={i}
+          data-cy={`exam-sessions-table-row-${i}`}
+          onClick={() => props.examSessionSelected(e)}
+        >
+          <p>{moment(e.session_date).format(DATE_FORMAT)}</p>
+          <p>{languageToString(e.language_code).toLowerCase()}</p>
+          <p>{levelDescription(e.level_code).toLowerCase()}</p>
+          <p className={classes.DesktopOnly}>
+            {moment(e.registration_start_date).format(DATE_FORMAT_WITHOUT_YEAR)}
+            &ndash;
+            {moment(e.registration_end_date).format(DATE_FORMAT)}
+          </p>
+          <p>
+            {registrationOpen
+              ? `${participantsCount.participants} / ${participantsCount.maxParticipants}`
+              : '-'}
+          </p>
+        </div>
+      );
+    });
 
   return (
     <>
       <div>
-        <div className={classes.TitleRow}>
-          <h2>{props.t('examSession.upcomingExamSessions')}</h2>
-        </div>
+        <h2>{props.t('examSession.upcomingExamSessions')}</h2>
         {upcomingExamSessions.length > 0 ? (
           <div className={classes.Grid} data-cy="exam-sessions-table">
             <h3>{props.t('common.examDate')}</h3>
@@ -63,9 +68,7 @@ export const upcomingAndPastExamSessions = props => {
         )}
       </div>
       <div>
-        <div className={classes.TitleRow}>
-          <h2>{props.t('examSession.pastExamSessions')}</h2>
-        </div>
+        <h2>{props.t('examSession.pastExamSessions')}</h2>
         {pastExamSessions.length > 0 ? (
           <div className={classes.Grid} data-cy="exam-sessions-table">
             <h3>{props.t('common.examDate')}</h3>
