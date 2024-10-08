afdc833fb23797c3bad7925f222b1827892b09af
Laura Ketola <laura.ketola@gofore.com>
2024-10-08 12:55:47 +0300

    Allowing creating exam sessions over a year into the future [deploy]

--- src/main/js/src/components/ExamSessionForm/ExamSessionForm.js
+++ src/main/js/src/components/ExamSessionForm/ExamSessionForm.js
@@ -207,11 +207,7 @@ const examSessionForm = props => {
   ) => {
     // Disable date filtering in development because test data is not dynamic
     return examDates
-      .filter(e => {
-        return process.env.NODE_ENV !== 'development'
-          ? moment(e.exam_date).isBefore(moment().add(1, 'year'))
-          : e.exam_date;
-      })
+      .filter(e => e.exam_date)
       .map(examDate => {
         const enabled = R.includes(
           { language_code: selectedLanguage, level_code: selectedLevel },
