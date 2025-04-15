059f8c436658f08167556fbe205ec0f54484ae7d
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-04-15 16:26:47 +0300

    Upgrade axios

--- src/main/js/src/axios.js
+++ src/main/js/src/axios.js
@@ -13,20 +13,9 @@ instance.interceptors.request.use(config => {
     'Caller-Id': `${OPH_OID}.yki`,
     CSRF: getCookie('CSRF'),
   };
-  // Include a lang parameter to API calls other than for fetching exam session data,
-  // The additional parameter interferes with caching of exam session responses,
-  // and the exam session responses contain details for multiple languages anyway.
-  if (
-    config.url &&
-    config.url.startsWith('/yki/api/exam-session') &&
-    config.method === 'get'
-  ) {
-    return config;
-  } else {
-    const lang = i18next.language;
-    config.params = { lang: lang ? lang : 'fi' };
-    return config;
-  }
+  const lang = i18next.language;
+  config.params = { lang: lang ? lang : 'fi' };
+  return config;
 });
 
 instance.interceptors.response.use(
